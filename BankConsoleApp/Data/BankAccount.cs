b2adf92f6f1aeb085e5b1051915f851183e159c8
212213 <marselmubaraksin231@mail.ru>
2023-12-06 12:51:53 +0300

    изменения

--- BankConsoleApp/Data/BankAccount.cs
+++ BankConsoleApp/Data/BankAccount.cs
@@ -6,31 +6,41 @@ using System.Threading.Tasks;
 
 namespace BankConsoleApp.Data
 {
+    public delegate void BalanceOperation(string msg);
     internal class BankAccount
     {
         private decimal _balance = 0;
 
-        public BankAccount(string accountNumber, string password)
+        public BankAccount(string accountNumber, string password, decimal balance = 1000)
         {
             AccountNumber = accountNumber;
             Password = password;
-        }
+            Balance = balance;
 
+            RegisterHandler(PrintOperationMessage);
+        }
+        public BalanceOperation? Operation;
         public decimal Balance { get { return _balance; } set { _balance = value; } }
         public string AccountNumber { get; set; }
         public string Password { get; set; }
+        public void RegisterHandler(BalanceOperation del)
+        {
+            Operation += del;
+        }
 
-        public delegate void BalanceOperation(BankAccount account, int total);
-        public void Translation(BankAccount account, int total)
+        private void PrintOperationMessage(string msg) => Console.WriteLine(msg);
+
+        public void Transaction(BankAccount account, int total)
         {
             if (_balance - total > 0)
             {
                 account.Balance += total;
                 _balance -= total;
+                Operation?.Invoke($"Операция выполнена успешно! Сумма в размере {total} переведена на счет: {account.AccountNumber}. Ваш текущий баланс: {_balance}");
             }
             else
             {
-                Console.WriteLine("Not enf money");
+                Operation?.Invoke($"Недостаточно средств для проведения операции. Ваш текущий баланс: {_balance}");
             }
         }
     }
--- BankConsoleApp/Program.cs
+++ BankConsoleApp/Program.cs
@@ -1,4 +1,5 @@
 ﻿using BankConsoleApp.Data;
 
-BankAccount account = new BankAccount("295812932", "qwerty");
-//проверка ветви
+BankAccount account1 = new BankAccount("295812932", "qwerty");
+BankAccount account2 = new BankAccount("112432144", "zxcvbn");
+account1.Transaction(account2, 599);
