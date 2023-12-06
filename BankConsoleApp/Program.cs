0d028b4bc05bf9e52ddcf22e653c3cf5c71ecac3
Koi Den <prostobydet@gmail.com>
2023-12-06 12:06:17 +0300

    first

--- BankConsoleApp/Program.cs
+++ BankConsoleApp/Program.cs
@@ -1,5 +1,4 @@
 ﻿using BankConsoleApp.Data;
 
-BankAccount account1 = new BankAccount("295812932", "qwerty");
-BankAccount account2 = new BankAccount("112432144", "zxcvbn");
-account1.Transaction(account2, 599);
+BankAccount account = new BankAccount("295812932", "qwerty");
+account.Balance = 100;
