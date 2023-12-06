31f407e52bbdc5c8c5bb68f4ccc0ec6575d83513
unsighera <eddy.weston@bk.ru>
2023-12-06 17:32:16 +0300

    added delegates

--- BankConsoleApp/Data/BankAccount.cs
+++ BankConsoleApp/Data/BankAccount.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace BankConsoleApp.Data
 {
     delegate void BalanceOperation(string msg);
+    delegate void BalanceTopUpDown(string action);
+    delegate void ChangePasswordOrBill(string msg);
     internal class BankAccount
     {
         private decimal _balance = 0;
@@ -18,8 +21,11 @@ namespace BankConsoleApp.Data
             Balance = balance;
 
             RegisterHandler(PrintOperationMessage);
+            RegisterHandlerTop(OperationTopUpDown);
         }
         public BalanceOperation? Operation;
+        public BalanceTopUpDown? OperationTopUpDown;
+        public ChangePasswordOrBill? OperationChange;
 
         public decimal Balance { get { return _balance; } set { _balance = value; } }
         public string AccountNumber { get; set; }
@@ -28,8 +34,20 @@ namespace BankConsoleApp.Data
         {
             Operation += del;
         }
-        private void PrintOperationMessage(string msg) => Console.WriteLine(msg);
 
+        public void RegisterHandlerTop(BalanceTopUpDown del)
+        {
+            OperationTopUpDown += del;
+        }
+        public void RegisterHandlerChange(ChangePasswordOrBill del)
+        {
+            OperationChange += del;
+        }
+
+
+        private void PrintOperationMessage(string msg) => Console.WriteLine(msg);
+        private void BalanceTopUpDown(string action) => Console.ReadLine();
+        private void ChangePassword(string msg) => Console.WriteLine(msg);
         public void Transaction(BankAccount account, int total)
         {
             if (_balance - total > 0)
@@ -41,6 +59,67 @@ namespace BankConsoleApp.Data
             else
             {
                 Operation?.Invoke($"Недостаточно средств, пополните свой баланс! Ваш текущий баланс: {_balance}");
+            }
+        }
+
+        public void TopUpDown(BankAccount account, int total)
+        {
+            Console.WriteLine($"Что вы хотите сделать? \n1 - Пополнить счет\n2 - Снять со счета");
+            string action = Console.ReadLine();
+
+            if (action == "1")
+            {
+                _balance += total;
+                Console.WriteLine($"Вы пополнили баланс {total}, ваш текущий баланс {_balance}");
+            }
+            else if (action == "2")
+            {
+                if (total > _balance)
+                {
+                    Console.WriteLine("Недостаточно средств");
+                }
+                else
+                {
+                    _balance -= total;
+                    Console.WriteLine($"Вы сняли со счета {total}, ваш текущий баланс {_balance}");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Неверный тип операции!");
+            }
+
+            OperationTopUpDown?.Invoke(action);
+        }
+
+        public void ChangePasswordOrBill()
+        {
+            Console.WriteLine($"Что вы хотите сделать?\n1 - Сменить парль\n2 - Сменить номер счета");
+            string action = Console.ReadLine();
+
+            if (action == "1")
+            {
+                string NewPassword = Console.ReadLine();
+                if (NewPassword == null) { Console.WriteLine("Пароль не может ббыть пустым!"); }
+                else 
+                { 
+                    Password = NewPassword;
+                    OperationChange?.Invoke("Вы успешно сменили пароль!");
+                }
+            }
+            else if (action == "2")
+            {
+                string NewBill = Console.ReadLine();
+                if (NewBill == null) { Console.WriteLine("Пароль не может ббыть пустым!"); }
+                else 
+                { 
+                    AccountNumber = NewBill;
+                    OperationChange?.Invoke("Вы успешно сменили пароль!");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Выбран неверный тип операции!");
             }
         }
     }
--- BankConsoleApp/Program.cs
+++ BankConsoleApp/Program.cs
@@ -1,6 +1,123 @@
 ﻿using BankConsoleApp.Data;
 
-BankAccount account1 = new BankAccount("295812932", "qwerty");
+BankAccount account1 = new BankAccount("987654321", "qwerty");
 BankAccount account2 = new BankAccount("123456789", "zxcvbn");
 
-account1.Transaction(account2, 999);
+Console.Write($"Авторизация\nВаш номер счета:");
+string LoginBill = Console.ReadLine();
+Console.WriteLine($"Ваш пароль:");
+string LoginPassword = Console.ReadLine();
+
+if ((LoginBill == account1.AccountNumber) || (LoginBill == account2.AccountNumber))
+{
+    if (LoginBill == account1.AccountNumber)
+    {
+        if (LoginPassword == account1.Password)
+        {
+            Console.WriteLine("Вход успешный!");
+            Menu();
+        }
+        else 
+        { 
+            Console.WriteLine("Неверный пароль!");
+        }
+    }
+    else if (LoginBill == account2.AccountNumber)
+    {
+        if (LoginPassword == account2.Password)
+        {
+            Console.WriteLine("Вход успешный!");
+            Menu();
+        }
+        else 
+        {
+            Console.WriteLine("Неверный пароль!");
+        }
+    }
+    else { Console.WriteLine("Неверные учетные данные!"); }
+}
+else { Console.WriteLine("Неверные учетные данные!"); }
+
+void Menu()
+{
+    bool exit = true;
+    Console.WriteLine($"Доброе пожалвать в банк. Наш список возможностей:\n1 - перевести деньги клиенту\n2 - снять наличные / пополнить баланс\n3 - сменить пароль или номер счета\n4 - выйти");
+    string action = Console.ReadLine();
+
+    while (exit)
+    {
+        if (action == "1")
+        {
+            Console.Write("Введите номер счета клиента, которму хотите перевести средства: ");
+            string AccountNumber = Console.ReadLine();
+            if (LoginBill == account1.AccountNumber)
+            {
+                if (AccountNumber == account2.AccountNumber)
+                {
+                    Console.WriteLine("Введите сумму транзацкии: ");
+                    int sum = Convert.ToInt32(Console.ReadLine());
+                    account1.Transaction(account2, sum);
+                    Menu();
+                    Console.Clear();
+                }
+            }
+            else
+            {
+                if (AccountNumber == account1.AccountNumber)
+                {
+                    Console.WriteLine("Введите сумму транзацкии: ");
+                    int sum = Convert.ToInt32(Console.ReadLine());
+                    account2.Transaction(account1, sum);
+                    Menu();
+                    Console.Clear();
+                }
+            }
+        }
+        else if (action == "2")
+        {
+            Console.Write("Введите ваш номер счета: ");
+            string AccountNumber = Console.ReadLine();
+            if (AccountNumber == account1.AccountNumber)
+            {
+                Console.WriteLine("Введите сумму списания / пополнения: ");
+                int sum = Convert.ToInt32(Console.ReadLine());
+                account1.TopUpDown(account1, sum);
+                Menu();
+                Console.Clear();
+            }
+            else
+            {
+                Console.WriteLine("Введите сумму списания / пополнения: ");
+                int sum = Convert.ToInt32(Console.ReadLine());
+                account2.TopUpDown(account2, sum);
+                Menu();
+                Console.Clear();
+            }
+        }
+        else if (action == "3")
+        {
+            Console.Write("Введите ваш номер счета: ");
+            string AccountNumber = Console.ReadLine();
+            if (AccountNumber == account1.AccountNumber)
+            {
+                account1.ChangePasswordOrBill();
+                Menu();
+                Console.Clear();
+            }
+            else
+            {
+                account2.ChangePasswordOrBill();
+                Menu();
+                Console.Clear();
+            }
+        }
+        else if (action == "4")
+        {
+            exit = false;
+        }
+        else
+        {
+            Console.WriteLine("Выбран неверный тип операции");
+        }
+    }
+}
