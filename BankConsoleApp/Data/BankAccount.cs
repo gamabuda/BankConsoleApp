8e76f6a3875c4b8dcbf87dc2e67c99cd5587ecf7
Сафиуллин Самат <samatsafiullin7@gmail.com>
2023-12-06 13:17:43 +0300

    Second

--- BankConsoleApp/Data/BankAccount.cs
+++ BankConsoleApp/Data/BankAccount.cs
@@ -6,27 +6,37 @@ using System.Threading.Tasks;
 
 namespace BankConsoleApp.Data
 {
+    public delegate void BalanceOperation(string msg);
     internal class BankAccount
     {
         private decimal _balance = 0;
 
-        public BankAccount(string accountNumber, string password)
+        public BankAccount(string accountNumber, string password, decimal balance = 1000)
         {
             AccountNumber = accountNumber;
             Password = password;
+            Balance = balance;
+            RegistrHandler(PrintOperationMessage);
         }
-
+        public BalanceOperation? Operation;
         public decimal Balance { get { return _balance; } set { _balance = value; } }
         public string AccountNumber { get; set; }
         public string Password { get; set; }
 
-        public delegate void BalanceOperation(BankAccount account, int total);
-        public void Translation(BankAccount account, int total)
+        public void RegistrHandler(BalanceOperation del)
+        {
+            Operation += del;
+        }
+        public void PrintOperationMessage(string msg) => Console.WriteLine(msg);
+
+        
+        public void Transition(BankAccount account, int total)
         {
             if (_balance - total > 0)
             {
                 account.Balance += total;
                 _balance -= total;
+                Operation?.Invoke($"Операция выполнена успешно! Сумма в размере {total} переведена на счет: {account.AccountNumber}");
             }
             else
             {
--- BankConsoleApp/Program.cs
+++ BankConsoleApp/Program.cs
@@ -1,3 +1,5 @@
 ﻿using BankConsoleApp.Data;
 
-BankAccount account = new BankAccount("295812932", "qwerty");
+BankAccount account1 = new BankAccount("295812932", "qwerty");
+BankAccount account2 = new BankAccount("2958342442312932", "qw3423432432erty");
+account1.Transition(account2, 200);
