d902be584e9bca2e9def057eed40c74f1c659407
Богдан Попов <bogdanpopov5570@gmail.com>
2023-12-11 12:05:18 +0300

    Добавлены делегаты снятия наличных со счета и обмен рублей на доллары

--- BankConsoleApp/Data/BankAccount.cs
+++ BankConsoleApp/Data/BankAccount.cs
@@ -6,41 +6,82 @@ using System.Threading.Tasks;
 
 namespace BankConsoleApp.Data
 {
-    public delegate void BalanceOperation(string msg);
+    public delegate void TransactionOperation(string msg);
+    public delegate void WithdrawalOperation(string msg);
+    public delegate void ExchangeOperation(string msg);
+
     internal class BankAccount
     {
         private decimal _balance = 0;
+        private decimal _dollarsBallance = 0;
 
-        public BankAccount(string accountNumber, string password, decimal balance = 1000)
+        public BankAccount(string accountNumber, string password, decimal balance = 1000, decimal rate = 91.02m, decimal dollarsBallance = 0)
         {
             AccountNumber = accountNumber;
             Password = password;
             Balance = balance;
+            Rate = rate;
+            DollarsBallance = dollarsBallance;
 
-            RegisterHandler(PrintOperationMessage);
+            TransactionOperationHandler(PrintOperationMessage);
+            WithdrawalOperationHandler(PrintOperationMessage);
+            ExchangeOperationHandler(PrintOperationMessage);
         }
-        public BalanceOperation? Operation;
+        public TransactionOperation? Operation;
+        public WithdrawalOperation? OperationWithdrawal;
+        public ExchangeOperation? OperationExchange;
+
         public decimal Balance { get { return _balance; } set { _balance = value; } }
         public string AccountNumber { get; set; }
         public string Password { get; set; }
-        public void RegisterHandler(BalanceOperation del)
-        {
-            Operation += del;
-        }
+        public decimal Rate { get; set; }
+        public decimal DollarsBallance { get { return _dollarsBallance; } set { _dollarsBallance = value; } }
+
+        public void TransactionOperationHandler(TransactionOperation del) => Operation += del;
+        public void WithdrawalOperationHandler(WithdrawalOperation del) => OperationWithdrawal += del;
+        public void ExchangeOperationHandler(ExchangeOperation del) => OperationExchange += del;
         
         private void PrintOperationMessage(string msg) => Console.WriteLine(msg);
 
         public void  Transaction(BankAccount account, int total)
         {
-            if (_balance - total > 0)
+            if (_balance - total >= 0)
             {
                 account.Balance += total;
                 _balance -= total;
-                Operation?.Invoke($"Операция выполнена успешно! Сумма в размере {total} переведена на счет: {account.AccountNumber}. Ваш текущий баланс: {_balance}");
+                Operation?.Invoke($"Операция выполнена успешно! Сумма в размере {total}₽ переведена на счет: {account.AccountNumber}. Ваш текущий баланс: {_balance}₽");
             }
             else
             {
                 Operation?.Invoke($"Недостаточно средств для проведения операции. Ваш текущий баланс: {_balance}"); 
+            }
+        }
+
+        public void Withdrawal(int total)
+        {
+            if (_balance - total >= 0)
+            {
+                _balance -= total;
+                OperationWithdrawal?.Invoke($"Операция выполнена успешно! Сумма в размере {total}₽ снята с Вашего счета. Ваш текущий баланс: {_balance}₽");
+            }
+            else
+            {
+                OperationWithdrawal?.Invoke($"Недостаточно средств для проведения операции. Ваш текущий баланс: {_balance}");
+            }
+        }
+
+        public void ExchangeCurrency(int total)
+        {
+            if (_balance - total >= 0)
+            {
+                _balance -= total;
+                decimal amount = Math.Round(total / Rate, 2, MidpointRounding.AwayFromZero);
+                _dollarsBallance += amount;
+                OperationWithdrawal?.Invoke($"Операция выполнена успешно! {total}₽ были обменены на {amount}$.\nВаш текущий баланс в рублях: {_balance}₽\nВаш текущий баланс в долларах: {_dollarsBallance}$");
+            }
+            else
+            {
+                OperationWithdrawal?.Invoke($"Недостаточно средств для проведения операции. Ваш текущий баланс: {_balance}");
             }
         }
     }
--- BankConsoleApp/Program.cs
+++ BankConsoleApp/Program.cs
@@ -1,5 +1,17 @@
-﻿using BankConsoleApp.Data;
+﻿using System.Collections.Generic;
+using System.Numerics;
+using System.Text;
+using BankConsoleApp.Data;
+
+Console.OutputEncoding = Encoding.UTF8;
 
 BankAccount account1 = new BankAccount("295812932", "qwerty");
 BankAccount account2 = new BankAccount("112432144", "zxcvbn");
+Console.WriteLine("Аккаунт 1. Операции:");
 account1.Transaction(account2, 599);
+account1.ExchangeCurrency(401);
+
+Console.WriteLine("\nАккаунт 2. Операции:");
+account2.Withdrawal(300);
+account2.ExchangeCurrency(600);
+account2.ExchangeCurrency(450);
