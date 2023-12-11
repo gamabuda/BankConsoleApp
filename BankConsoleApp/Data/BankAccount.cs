99d3f0007047f64d1599cbb89ebf665bdb616aa1
Богдан Попов <bogdanpopov5570@gmail.com>
2023-12-11 16:26:00 +0300

    Добавлены события на изменение валютного счета, обнуление рублевого счета и открытие нового счета

--- BankConsoleApp/Data/BankAccount.cs
+++ BankConsoleApp/Data/BankAccount.cs
@@ -14,34 +14,62 @@ namespace BankConsoleApp.Data
     {
         private decimal _balance = 0;
         private decimal _dollarsBallance = 0;
-
-        public BankAccount(string accountNumber, string password, decimal balance = 1000, decimal rate = 91.02m, decimal dollarsBallance = 0)
+        public string AccountNumber { get; set; }
+        public string Password { get; set; }
+        public decimal Rate { get; set; }
+        public decimal DollarsBallance
         {
-            AccountNumber = accountNumber;
+            get => _dollarsBallance;
+            set
+            {
+                _dollarsBallance = value;
+            }
+        }
+
+        public BankAccount(string accountNumber, string password, decimal balance = 1000, decimal rate = 91.02m, decimal dollarBalance = 0)
+        {
+            AccountNumber = accountNumber; 
             Password = password;
             Balance = balance;
             Rate = rate;
-            DollarsBallance = dollarsBallance;
+            DollarsBallance = dollarBalance;
 
             TransactionOperationHandler(PrintOperationMessage);
             WithdrawalOperationHandler(PrintOperationMessage);
             ExchangeOperationHandler(PrintOperationMessage);
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            PrintData?.Invoke($"Открыт новый счет! Номер: {accountNumber} | Пароль: {password} | Баланс (₽): {balance} | Баланс ($): {dollarBalance}");
+            Console.ResetColor();
         }
         public TransactionOperation? Operation;
         public WithdrawalOperation? OperationWithdrawal;
         public ExchangeOperation? OperationExchange;
 
-        public decimal Balance { get { return _balance; } set { _balance = value; } }
-        public string AccountNumber { get; set; }
-        public string Password { get; set; }
-        public decimal Rate { get; set; }
-        public decimal DollarsBallance { get { return _dollarsBallance; } set { _dollarsBallance = value; } }
+        public delegate void AccountHandler(string msg);
+
+        public event AccountHandler? CurancyAccountChanged = PrintOperationMessage;
+        public event AccountHandler? Bankruptcy = PrintOperationMessage;
+        public event AccountHandler? PrintData = PrintOperationMessage;
+        public decimal Balance { 
+            get => _balance;
+            set 
+            {
+                _balance = value; 
+                if (_balance == 0)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Bankruptcy?.Invoke("Ваш рублевый счет обнулился!");
+                    Console.ResetColor();
+                }
+            } 
+        }
 
         public void TransactionOperationHandler(TransactionOperation del) => Operation += del;
         public void WithdrawalOperationHandler(WithdrawalOperation del) => OperationWithdrawal += del;
         public void ExchangeOperationHandler(ExchangeOperation del) => OperationExchange += del;
         
-        private void PrintOperationMessage(string msg) => Console.WriteLine(msg);
+        private static void PrintOperationMessage(string msg) => Console.WriteLine(msg);
 
         public void  Transaction(BankAccount account, int total)
         {
@@ -78,6 +106,10 @@ namespace BankConsoleApp.Data
                 decimal amount = Math.Round(total / Rate, 2, MidpointRounding.AwayFromZero);
                 _dollarsBallance += amount;
                 OperationWithdrawal?.Invoke($"Операция выполнена успешно! {total}₽ были обменены на {amount}$.\nВаш текущий баланс в рублях: {_balance}₽\nВаш текущий баланс в долларах: {_dollarsBallance}$");
+
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                CurancyAccountChanged?.Invoke("Изменен баланс валютного счета!");
+                Console.ResetColor();
             }
             else
             {
--- BankConsoleApp/Program.cs
+++ BankConsoleApp/Program.cs
@@ -5,13 +5,16 @@ using BankConsoleApp.Data;
 
 Console.OutputEncoding = Encoding.UTF8;
 
-BankAccount account1 = new BankAccount("295812932", "qwerty");
-BankAccount account2 = new BankAccount("112432144", "zxcvbn");
-Console.WriteLine("Аккаунт 1. Операции:");
+BankAccount account1 = new BankAccount("000001", "qwerty");
+BankAccount account2 = new BankAccount("000002", "zxcvbn");
+Console.WriteLine($"\nСчет {account1.AccountNumber}. Операции:");
 account1.Transaction(account2, 599);
 account1.ExchangeCurrency(401);
+account1.Balance = 0;
 
-Console.WriteLine("\nАккаунт 2. Операции:");
+Console.WriteLine("\n------------------------------------------------------------------------------------------------");
+
+Console.WriteLine($"\nСчет {account2.AccountNumber}. Операции:");
 account2.Withdrawal(300);
 account2.ExchangeCurrency(600);
 account2.ExchangeCurrency(450);
