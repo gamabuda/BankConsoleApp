febd0b55f77c35ed245bc1dad6950abceb13c566
bogdanpopov1 <bogdan_popov17@mail.ru>
2023-12-13 13:18:52 +0300

    1

--- BankConsoleApp/Program.cs
+++ BankConsoleApp/Program.cs
@@ -5,15 +5,16 @@ using BankConsoleApp.Data;
 
 Console.OutputEncoding = Encoding.UTF8;
 
-BankAccount account1 = new BankAccount("000001", "qwerty");
-BankAccount account2 = new BankAccount("000002", "zxcvbn");
-Console.WriteLine($"\nСчет {account1.AccountNumber}. Операции:");
-account1.Transaction(account2, 599);
-account1.ExchangeCurrency(401);
+//BankAccount account1 = new BankAccount("000001", "qwerty");
+//BankAccount account2 = new BankAccount("000002", "zxcvbn");
+//Console.WriteLine($"\nСчет {account1.AccountNumber}. Операции:");
+//account1.Transaction(account2, 599);
+//account1.ExchangeCurrency(401);
 
-Console.WriteLine("\n------------------------------------------------------------------------------------------------");
+//Console.WriteLine("\n------------------------------------------------------------------------------------------------");
 
-Console.WriteLine($"\nСчет {account2.AccountNumber}. Операции:");
-account2.Withdrawal(300);
-account2.ExchangeCurrency(600);
-account2.ExchangeCurrency(450);
+//Console.WriteLine($"\nСчет {account2.AccountNumber}. Операции:");
+//account2.Withdrawal(300);
+//account2.ExchangeCurrency(600);
+//account2.ExchangeCurrency(450);
+
