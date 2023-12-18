07674da1a2c3f4962b046b92522f1b7b60e04e38
Марсель Мубаракшин <marselmubaraksin231@mail.ru>
2023-12-18 15:21:45 +0300

    транспорт

--- BankConsoleApp/Program.cs
+++ BankConsoleApp/Program.cs
@@ -1,8 +1,19 @@
 ﻿using BankConsoleApp.Data;
+using BankConsoleApp.transport;
+using System;
 
 BankAccount account1 = new BankAccount("212213", "qwerty");
 BankAccount account2 = new BankAccount("01050657", "zxcvbn");
+BankAccount TransportArenda = new BankAccount("01050657", "zxcvbn");
 
+Car Volga = new Car("Volga", "While", 95, true, "B", 4, 500);
+Car Niva = new Car("Niva", "Green", 130, true, "B", 4, 500);
+
+
+List<Transport> transports = new List<Transport>();
+
+transports.Add(Volga);
+transports.Add(Niva);
 
 Console.Write($"Вход в систему банка C# \nВведите Ваш номер счета:");
 string Entrance = Console.ReadLine();
@@ -38,11 +49,43 @@ if ((Entrance == account1.AccountNumber) || (Entrance == account2.AccountNumber)
 }
 else { Console.WriteLine("Неверные учетные данные!"); }
 
+void SelectTransport(List<Transport> transports)
+{
+    Console.WriteLine("Выберите транспорт по индексу:");
+    for (int i = 0; i < transports.Count; i++)
+    {
+        Console.WriteLine($"{i + 1}. {transports[i].Model}");
+    }
+
+    int selectedIndex;
+    while (true)
+    {
+        if (int.TryParse(Console.ReadLine(), out selectedIndex) && selectedIndex > 0 && selectedIndex <= transports.Count)
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Введите корректный индекс");
+        }
+    }
+
+    Console.WriteLine($"Вы выбрали: {transports[selectedIndex - 1].Model}");
+    transports[selectedIndex - 1].CheckRentalStatus();
+
+    Console.WriteLine($"Цена ареды за {transports[selectedIndex - 1].Model} составляет {transports[selectedIndex - 1].Price} руб.");
+    if (Entrance == account1.AccountNumber)
+    {
+        account1.Transaction(TransportArenda, Convert.ToInt32(transports[selectedIndex - 1].Price));
+    }
+    else { account2.Transaction(TransportArenda, Convert.ToInt32(transports[selectedIndex - 1].Price)); }
+}
+
 
 void Menu()
 {
     bool exit = true;
-    Console.WriteLine($"Банк C# рад приветствовать вас.  Ваш список возможностей:\n1 - перевести деньги клиенту\n2 - снять наличные / пополнить баланс\n3 - сменить пароль или номер счета\n4 - выйти");
+    Console.WriteLine($"Банк C# рад приветствовать вас.  Ваш список возможностей:\n1 - перевести деньги клиенту\n2 - снять наличные / пополнить баланс\n3 - сменить пароль или номер счета\n4 - Арендовать транспорт\n5 - Выйти");
     string operation = Console.ReadLine();
 
     while (exit)
@@ -107,12 +150,18 @@ void Menu()
             }
         }
         else if (operation == "4")
-        {
-            exit = false;
+        { 
+            Console.WriteLine("Ассортимент наших транспортов под аренду:");
+            SelectTransport(transports);
         }
-        else
-        {
-            Console.WriteLine("Выбран неверный тип операции");
-        }
+
+        else if (operation == "5")
+    {
+        exit = false;
     }
+    else
+    {
+        Console.WriteLine("Выбран неверный тип операции");
+    }
+}
 }
---
+++ BankConsoleApp/transport/car.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankConsoleApp.transport
+{
+    public class Car : Transport
+    {
+        public event TransportInfo? Information;
+        public string Model { get; set; }
+        public string Color { get; set; }
+        public int MaxSpeed { get; set; }
+        public bool IsRented { get; set; }
+        public string Category { get; set; }
+        public int CountOfWheels { get; set; }
+        public decimal Price { get; }
+
+
+        public Car(string model, string color, int maxspeed, bool isrented, string category, int countofwheels, decimal price)
+
+        {
+            Model = model;
+            Color = color;
+            MaxSpeed = maxspeed;
+            IsRented = isrented;
+            Category = category;
+            CountOfWheels = countofwheels;
+            Price = price;
+        }
+        public void PrintInfo(string msg)
+        {
+            Console.WriteLine($"Car - Model: {Model}, Color: {Color}, Max Speed: {MaxSpeed}, Category: {Category}, Is Rented: {IsRented}, Count of Wheels: {CountOfWheels}");
+        }
+    }
+}
